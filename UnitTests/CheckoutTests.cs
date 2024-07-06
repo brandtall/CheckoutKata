1cf84d4fbb2c3d417b7f50f81757824fb4dd5299
Brandtall <webyahyaver@gmail.com>
2024-07-06 13:24:06 +0400

    add test

--- UnitTests/CheckoutTests.cs
+++ UnitTests/CheckoutTests.cs
@@ -5,19 +5,36 @@ public class Tests
     [Test]
     public void ShouldReturnTotal_NoOffers()
     {
-        var sut = new Checkout();
-
+        Dictionary<string, double> pricing = new Dictionary<string, double>();
+        pricing.Add("A", 4);
+        pricing.Add("B", 6);
+        pricing.Add("C", 8.2);
+        
+        
+        var sut = new Checkout(pricing);
         var total = sut.Total();
         
-        Assert.NotNull(total);
+        
+        Assert.That(total, Is.EqualTo(4 + 6 + 8.2));
     }
     
 }
 
 public class Checkout
 {
-    public int Total()
+    private readonly double _sum;
+
+    public Checkout(Dictionary<string,double>? pricing)
     {
-        return 0;
+        if (pricing != null)
+            foreach (var price in pricing.Values)
+            {
+                _sum += price;
+            }
+    }
+
+    public double Total()
+    {
+        return _sum;
     }
 }
