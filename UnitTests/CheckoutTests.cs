e43fd7816ba706d4e4a2547505cb997b15953e44
Brandtall <webyahyaver@gmail.com>
2024-07-06 13:47:18 +0400

    fix tests (add items)

--- UnitTests/CheckoutTests.cs
+++ UnitTests/CheckoutTests.cs
@@ -3,45 +3,51 @@ namespace UnitTests;
 public class Tests
 {
     [Test]
-    public void ShouldReturnTotal_NoOffers()
-    {
-        Dictionary<string, double> pricing = new Dictionary<string, double>();
-        pricing.Add("A", 4);
-        pricing.Add("B", 6);
-        pricing.Add("C", 8.2);
-        
-        
-        var sut = new Checkout(pricing);
-        var total = sut.Total();
-        
-        
-        Assert.That(total, Is.EqualTo(4 + 6 + 8.2));
-    }
-    
-    [Test]
     public void ShouldReturnZero_WhenNoPricesAreSet()
     {
         Dictionary<string, double> pricing = new Dictionary<string, double>();
+        Dictionary<string, int>? items = new Dictionary<string, int>();
         
-        var sut = new Checkout(pricing);
+        var sut = new Checkout(items, pricing);
         var total = sut.Total();
         
         
         Assert.That(total, Is.EqualTo(0));
     }
-    
+
+    [Test]
+    public void ShouldReturnTotal_WithNoOffers()
+    {
+        Dictionary<string, double> pricing = new Dictionary<string, double>();
+        Dictionary<string, int>? items = new Dictionary<string, int>();
+
+        pricing.Add("A", 4);
+        pricing.Add("B", 6);
+        pricing.Add("C", 8.2);
+        
+        items.Add("A", 1);
+        items.Add("B", 1);
+        items.Add("C", 1);
+        
+        
+        var sut = new Checkout(items, pricing);
+        var total = sut.Total();
+        
+        
+        Assert.That(total, Is.EqualTo(4 + 6 + 8.2));
+    }
 }
 
 public class Checkout
 {
     private readonly double _sum;
 
-    public Checkout(Dictionary<string,double>? pricing)
+    public Checkout(Dictionary<string, int>? items, Dictionary<string, double>? pricing)
     {
-        if (pricing == null) return;
-        foreach (var price in pricing.Values)
+        if (pricing is null || items is null) return;
+        foreach (var item in pricing)
         {
-            _sum += price;
+            _sum += items[item.Key]*item.Value;
         }
     }
 
